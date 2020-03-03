f8bbd3109666478c38e36d833d71ca2ccb3ace24
george <gnicolaou@axiomc.com>
2020-03-03 16:12:53 +0200

    editing to support versions < iOS 13

--- GNImageProgressBarDemo/ContentView.swift
+++ GNImageProgressBarDemo/ContentView.swift
@@ -8,12 +8,14 @@
 
 import SwiftUI
 
+@available(iOS 13.0, *)
 struct ContentView: View {
     var body: some View {
         Text("Hello, World!")
     }
 }
 
+@available(iOS 13.0, *)
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
