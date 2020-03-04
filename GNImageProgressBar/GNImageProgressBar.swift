77f6fa079cfc3d0fab9df00d35b8a6f66a14f089
george <gnicolaou@axiomc.com>
2020-03-04 15:42:21 +0200

    update code to take image aspect size into consideration

--- GNImageProgressBar/GNImageProgressBar.swift
+++ GNImageProgressBar/GNImageProgressBar.swift
@@ -16,7 +16,6 @@ public class GNImageProgressBar: UIView {
     private lazy var backgroundImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .center
-        iv.image = image
         iv.translatesAutoresizingMaskIntoConstraints = false
         return iv
     }()
@@ -28,21 +27,24 @@ public class GNImageProgressBar: UIView {
             iv.contentMode = .bottom
         }
         iv.clipsToBounds = true
-        iv.image = image
         return iv
     }()
     
     private lazy var trackIvHeightConstraint = NSLayoutConstraint(item: trackImageView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 0)
     
     public var image: UIImage
+    public var size: CGSize
     public var shouldShowBackgroundImage: Bool
     public var backgroundImageAlpha: CGFloat
     public var fillDirection: FillDirection
     public var progress: CGFloat
     
+    public var fixedSize: CGSize = .zero
+    
     
     public init(image: UIImage, size: CGSize, backgroundImageAlpha: CGFloat = 0.4, shouldShowBackgroundImage: Bool = true, fillDirection: FillDirection, progress: CGFloat = 0) {
-        self.image = image.resizedImageWithinRect(rectSize: size)
+        self.image = image
+        self.size = size
         self.shouldShowBackgroundImage = shouldShowBackgroundImage
         self.backgroundImageAlpha = shouldShowBackgroundImage ? backgroundImageAlpha : 0
         self.fillDirection = fillDirection
@@ -73,8 +75,49 @@ public class GNImageProgressBar: UIView {
         addSubview(backgroundImageView)
         addSubview(trackImageView)
         
+        setupImageViews()
         initialConstraints()
     }
+    
+    
+    private func setupImageViews() {
+        fixedSize = sizeOfAspectFit(imageSize: image.size, containerSize: size)
+        backgroundImageView.image = image.resizedImageWithinRect(rectSize: fixedSize)
+        trackImageView.image = image.resizedImageWithinRect(rectSize: fixedSize)
+    }
+    
+    private func initialConstraints() {
+        /// edges equal to superview
+        NSLayoutConstraint(item: backgroundImageView, attribute: .top, relatedBy: .greaterThanOrEqual, toItem: self, attribute: .top, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: backgroundImageView, attribute: .bottom, relatedBy: .lessThanOrEqual, toItem: self, attribute: .bottom, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: backgroundImageView, attribute: .left, relatedBy: .greaterThanOrEqual, toItem: self, attribute: .left, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: backgroundImageView, attribute: .right, relatedBy: .lessThanOrEqual, toItem: self, attribute: .right, multiplier: 1, constant: 0).isActive = true
+        
+        backgroundImageView.heightAnchor.constraint(equalToConstant: fixedSize.height).isActive = true
+        backgroundImageView.widthAnchor.constraint(equalToConstant: fixedSize.width).isActive = true
+    }
+
+    private func animateFromBottomConstraints(_ height: CGFloat) {
+        trackIvHeightConstraint.constant = height
+        trackIvHeightConstraint.isActive = true
+
+        NSLayoutConstraint(item: trackImageView, attribute: .bottom, relatedBy: .equal, toItem: backgroundImageView, attribute: .bottom, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: trackImageView, attribute: .left, relatedBy: .equal, toItem: backgroundImageView, attribute: .left, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: trackImageView, attribute: .right, relatedBy: .equal, toItem: backgroundImageView, attribute: .right, multiplier: 1, constant: 0).isActive = true
+    }
+    
+    func sizeOfAspectFit(imageSize: CGSize, containerSize: CGSize) -> CGSize {
+        var aspectFitSize = CGSize(width: containerSize.width, height: containerSize.height)
+        let mW = containerSize.width / imageSize.width
+        let mH = containerSize.height / imageSize.height
+        if mH < mW {
+            aspectFitSize.width = mH * imageSize.width
+        } else if mW < mH {
+            aspectFitSize.height = mW * imageSize.height
+        }
+        return aspectFitSize
+    }
+    
     
     public func setProgress(_ progress: CGFloat, animated: Bool, duration: TimeInterval = 0.2) {
         self.progress = progress
@@ -89,24 +132,6 @@ public class GNImageProgressBar: UIView {
             self.layoutIfNeeded()
         }, completion: { _ in
         })
-    }
-    
-    
-    private func initialConstraints() {
-        /// edges equal to superview
-        NSLayoutConstraint(item: backgroundImageView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0).isActive = true
-        NSLayoutConstraint(item: backgroundImageView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0).isActive = true
-        NSLayoutConstraint(item: backgroundImageView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1, constant: 0).isActive = true
-        NSLayoutConstraint(item: backgroundImageView, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1, constant: 0).isActive = true
-    }
-
-    private func animateFromBottomConstraints(_ height: CGFloat) {
-        trackIvHeightConstraint.constant = height
-        trackIvHeightConstraint.isActive = true
-
-        NSLayoutConstraint(item: trackImageView, attribute: .bottom, relatedBy: .equal, toItem: backgroundImageView, attribute: .bottom, multiplier: 1, constant: 0).isActive = true
-        NSLayoutConstraint(item: trackImageView, attribute: .left, relatedBy: .equal, toItem: backgroundImageView, attribute: .left, multiplier: 1, constant: 0).isActive = true
-        NSLayoutConstraint(item: trackImageView, attribute: .right, relatedBy: .equal, toItem: backgroundImageView, attribute: .right, multiplier: 1, constant: 0).isActive = true
     }
 }
 
--- GNImageProgressBarDemo/ExampleViewController.swift
+++ GNImageProgressBarDemo/ExampleViewController.swift
@@ -13,10 +13,9 @@ class ExampleViewController: UIViewController {
     
     let width = min(#imageLiteral(resourceName: "united_logo").size.width, UIScreen.main.bounds.width - 40)
     let height = min(#imageLiteral(resourceName: "united_logo").size.height, UIScreen.main.bounds.height - 80)
-    lazy var minDimension = min(width, height)
     
     lazy var imageProgressBar: GNImageProgressBar = {
-        let b = GNImageProgressBar(image: #imageLiteral(resourceName: "united_logo"), size: CGSize(width: minDimension, height: minDimension), backgroundImageAlpha: 0.3, shouldShowBackgroundImage: true, fillDirection: .fromBottom, progress: 0.3)
+        let b = GNImageProgressBar(image: #imageLiteral(resourceName: "united_logo"), size: CGSize(width: width, height: height), backgroundImageAlpha: 0.3, shouldShowBackgroundImage: true, fillDirection: .fromBottom, progress: 0.3)
         b.translatesAutoresizingMaskIntoConstraints = false
         return b
     }()
@@ -36,8 +35,6 @@ class ExampleViewController: UIViewController {
     func setupConstraints() {
         NSLayoutConstraint(item: imageProgressBar, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1, constant: 0).isActive = true
         NSLayoutConstraint(item: imageProgressBar, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 1, constant: 0).isActive = true
-        NSLayoutConstraint(item: imageProgressBar, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: minDimension).isActive = true
-        NSLayoutConstraint(item: imageProgressBar, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: minDimension).isActive = true
     }
     
     @objc func viewTapped() {
